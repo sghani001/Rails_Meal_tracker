c3c8b49d1457b27a49b23b88199b447f85ca06d8
sghani001 <2020cs669@student.uet.edu.pk>
2024-08-07 13:47:59 +0500

    button_add

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import 'bootstrap'
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("hello from application.js");
+    // Example DOM manipulation
+    const button = document.getElementById("check-button");
+    button.addEventListener("click", () => {
+      const element = document.getElementById("hello");
+      element.textContent = "You clicked the button!";
+    });
+  });
